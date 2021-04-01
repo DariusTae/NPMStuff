864d6ad28e42e409b927e496040950e139820228
DariusFreeman <DariusTae@gmail.com>
2021-04-01 10:22:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let faker = require('faker');
+
+console.log(faker.name.findName());
+console.log(faker.name.findName());console.log(faker.name.findName());console.log(faker.name.findName());console.log(faker.name.findName());console.log(faker.name.findName());console.log(faker.name.findName());
